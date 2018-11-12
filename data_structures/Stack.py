64c3dfc15e4ef308a44c77f73e1f39c2555a6d5c
Richard L Huynh <huynhtastic@gmail.com>
2018-11-12 08:57:18 -0600

    created stack data structure

---
+++ data_structures/Stack.py
@@ -0,0 +1,19 @@
+class Stack():
+    def __init__(self):
+        self.stack = []
+
+    def pop(self):
+        if not self.stack.is_empty():
+            return self.stack.pop()
+        raise ValueError('The stack is empty.')
+
+    def push(self, item):
+        self.stack.append(item)
+
+    def peek(self):
+        if not self.stack.is_empty():
+            return self.stack[-1]
+        raise ValueError('The stack is empty.')
+
+    def is_empty(self):
+        return bool(self.stack)
