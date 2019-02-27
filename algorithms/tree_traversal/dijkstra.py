9f965ec2818883118a6f4876cbf5c76ae20fd3f9
Richard L Huynh <huynhtastic@gmail.com>
2019-02-27 11:03:50 -0600

    finished disjkstra

--- algorithms/tree_traversal/dijkstra.py
+++ algorithms/tree_traversal/dijkstra.py
@@ -12,14 +12,21 @@ def dijkstra(root):
 
     travelled_dist = 0
     while to_visit:
+        # extract current node info
         cur_node, prev_node, travelled_dist = to_visit.pop()
-        print(visited_edges)
+        #print(visited_edges)
         visited_edges.append({cur_node, prev_node})  # travelled this edge
+        # check if we've been to this node before from a different route
         if cur_node in visited:
+            # check if we went through this with a diff distance
             if travelled_dist < visited[cur_node][1]:
                 visited[cur_node] = (prev_node, travelled_dist)
         else:
+            # if we haven't been to this node, we just record that we've
+            # now travelled here before
             visited[cur_node] = (prev_node, travelled_dist)
+        # look through the neighbors and enqueue the nodes that we haven't
+        # been to before from this current node
         for neighbor, dist in cur_node.adjacents:
             if {cur_node, neighbor} not in visited_edges:
                 to_visit.append((neighbor, cur_node, travelled_dist + dist))
